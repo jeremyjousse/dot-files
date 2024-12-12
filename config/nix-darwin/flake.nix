bcf1844f29b4e6ee6484bce131aa782414bbb2ed
Jérémy Jousse <jeremy.jousse@decathlon.com>
2024-12-12 20:02:42 +0100

    feat(nix): :sparkles: update nix config

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "GrM's nix-darwin configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -7,55 +7,69 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.neovim
-          pkgs.eza
-          pkgs.mas
-          pkgs.nixfmt-rfc-style
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.neovim
+            pkgs.helix
+            pkgs.eza
+            pkgs.mas
+            pkgs.nixfmt-rfc-style
+            pkgs.zoxide
+          ];
 
-        ];
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          system.defaults = {
+            # dock.autohide = true;
+            # dock.mru-spaces = false;
+            # finder.AppleShowAllExtensions = true;
+            # finder.FXPreferredViewStyle = "clmv";
+            # loginwindow.LoginwindowText = "devops-toolbox";
+            screencapture.location = "~/Pictures/screenshots";
+            # screensaver.askForPasswordDelay = 10;
+          };
 
-      system.defaults = {
-        # dock.autohide = true;
-        # dock.mru-spaces = false;
-        # finder.AppleShowAllExtensions = true;
-        # finder.FXPreferredViewStyle = "clmv";
-        # loginwindow.LoginwindowText = "devops-toolbox";
-        screencapture.location = "~/Pictures/screenshots";
-        # screensaver.askForPasswordDelay = 10;
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "cowsay"
+            ];
+          };
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#$(scutil --get LocalHostName)
+      darwinConfigurations."FRL-Y65NQL4JTG" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
       };
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      darwinConfigurations."MacBook-Pro-de-Jeremy" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#$(scutil --get LocalHostName)
-    darwinConfigurations."FRL-Y65NQL4JTG" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-    darwinConfigurations."MacBook-Pro-de-Jeremy" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
