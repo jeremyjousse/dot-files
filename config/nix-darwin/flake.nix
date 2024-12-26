525be7f28585103f23b820ad5da7abb1e8425443
Jérémy Jousse <jeremy.jousse@decathlon.com>
2024-12-26 21:46:50 +0100

    feat(nix): :sparkles: add home-manager

--- config/nix-darwin/flake.nix
+++ config/nix-darwin/flake.nix
@@ -5,12 +5,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{
       self,
       nix-darwin,
+      home-manager,
       nixpkgs,
     }:
     let
