8ed1dc13f91080f612f3ecb0e45c2470c9a9c343
Jérémy Jousse <jeremy.jousse@gmail.com>
2025-12-02 17:19:50 +0100

    fix(install): add double quotes for zsh syntax highlighting plugin

--- lib/install.sh
+++ lib/install.sh
@@ -35,7 +35,7 @@ fi
 # OH My ZSH syntax highlighting (https://github.com/zsh-users/zsh-syntax-highlighting) Fish shell like syntax highlighting for Zsh.
 if [ ! -d "$HOME"/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
 	info "Install OH My syntax highlighting"
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 fi
 
 ###
