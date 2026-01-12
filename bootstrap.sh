f2780b178fcdd6408e95ea29a8b6780675031ec4
Jérémy Jousse <jeremy.jousse@decathlon.com>
2026-01-12 09:04:57 +0100

    feat(ai): :sparkles: add new gemeni commands and bootstrap them

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,15 @@ create_folder "$HOME"/Development/Personal
 link_config_files "$PWD"/config/zsh/.zshrc "$HOME"/.zshrc
 link_config_files "$PWD"/config/zsh "$HOME"/.config/zsh
 
-link_config_files "$PWD"/config/nushell "$HOME/Library/Application Support/nushell"
-
+link_config_files "$PWD"/ai/gemini/commands "$HOME"/.gemini/commands
 link_config_files "$PWD"/config/alacritty "$HOME"/.config/alacritty
+link_config_files "$PWD"/config/ghostty "$HOME"/.config/ghostty
 link_config_files "$PWD"/config/git/.gitconfig "$HOME"/.gitconfig
 link_config_files "$PWD"/config/hammerspoon "$HOME"/.config/hammerspoon
+link_config_files "$PWD"/config/nushell "$HOME/Library/Application Support/nushell"
 link_config_files "$PWD"/config/nvim "$HOME"/.config/nvim
 link_config_files "$PWD"/config/starship/starship.toml "$HOME"/.config/starship.toml
 link_config_files "$PWD"/config/zellij "$HOME"/.config/zellij
-link_config_files "$PWD"/config/ghostty "$HOME"/.config/ghostty
 
 copy_config_files "$PWD"/config/mise "$HOME"/.config
 
