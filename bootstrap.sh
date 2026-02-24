225d90cbc3f526d50b59e4080013f001b3dd33ab
Jérémy Jousse <jeremy.jousse@gmail.com>
2026-02-24 16:45:03 +0100

    refactor(bootstrap): :sparkles: reorder link config

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,8 @@ link_config_files "$PWD"/config/zsh "$HOME"/.config/zsh
 
 link_config_files "$PWD"/ai/gemini/commands "$HOME"/.gemini/commands
 link_config_files "$PWD"/ai/skills "$HOME"/.gemini/skills
-link_config_files "$PWD"/config/alacritty "$HOME"/.config/alacritty
 link_config_files "$PWD"/config/gemini/settings.json "$HOME"/.gemini/settings.json
+link_config_files "$PWD"/config/alacritty "$HOME"/.config/alacritty
 link_config_files "$PWD"/config/ghostty "$HOME"/.config/ghostty
 link_config_files "$PWD"/config/git/.gitconfig "$HOME"/.gitconfig
 link_config_files "$PWD"/config/hammerspoon "$HOME"/.config/hammerspoon
